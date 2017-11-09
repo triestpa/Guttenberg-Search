cd887b1a999bb3af0d1a4a872cb5342eb804aed4
triestpa <patrick.triest@gmail.com>
2017-11-09 15:00:38 +0530

    Add "getParagraphs" ES query

--- search.js
+++ search.js
@@ -1,13 +1,34 @@
 const { client, index, type } = require('./connection')
 
 module.exports = {
-  async search (term) {
+  async search (term, offset = 0) {
     const result = await client.search({
       index,
       type,
       body: {
+        from: offset,
         query: { match: { 'Text': term } },
         highlight: { fields: { Text: {} } }
+      }
+    })
+
+    return result
+  },
+  async getParagraphs (bookTitle, startLocation, endLocation) {
+    const result = await client.search({
+      index,
+      type,
+      body: {
+        size: endLocation - startLocation,
+        sort: { 'Paragraph': 'asc' },
+        query: {
+          bool: {
+            filter: [
+              { term: { 'Title': bookTitle } },
+              { range: { 'Paragraph': { gte: startLocation, lte: endLocation } } }
+            ]
+          }
+        }
       }
     })
 
