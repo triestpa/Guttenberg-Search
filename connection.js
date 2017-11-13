94d10c93cfeceac890b760bdc4104336fbb19275
triestpa <patrick.triest@gmail.com>
2017-11-13 07:59:44 +0545

    Move index setup from data loading script to ES connection script

--- connection.js
+++ connection.js
@@ -1,5 +1,8 @@
 const elasticsearch = require('elasticsearch')
 
+const index = 'library'
+const type = 'novel'
+
 const client = new elasticsearch.Client({
   host: 'localhost:9200'
   // log: 'trace'
@@ -7,8 +10,23 @@ const client = new elasticsearch.Client({
 
 client.cluster.health({}).then(console.log).catch(console.error)
 
+async function resetIndex () {
+  if (await client.indices.exists({ index })) {
+    await client.indices.delete({ index })
+  }
+
+  await client.indices.create({ index })
+
+  const schema = {
+    Title: { type: 'keyword' },
+    Author: { type: 'keyword' },
+    Paragraph: { type: 'integer' },
+    Text: { type: 'text' }
+  }
+
+  await client.indices.putMapping({ index, type, body: { properties: schema } })
+}
+
 module.exports = {
-  client,
-  index: 'library',
-  type: 'novel'
+  client, index, type, resetIndex
 }
--- load_data.js
+++ load_data.js
@@ -37,23 +37,6 @@ function parseBookFile (filePath) {
   return { title, author, sections }
 }
 
-async function resetIndex () {
-  if (await client.indices.exists({ index })) {
-    await client.indices.delete({ index })
-  }
-
-  await client.indices.create({ index })
-
-  const schema = {
-    Title: { type: 'keyword' },
-    Author: { type: 'keyword' },
-    Paragraph: { type: 'integer' },
-    Text: { type: 'text' }
-  }
-
-  await client.indices.putMapping({ index, type, body: { properties: schema } })
-}
-
 async function insertBookData (title, author, sections) {
   const bulkOps = []
   for (let i = 0; i < sections.length; i++) {
@@ -74,7 +57,7 @@ async function insertBookData (title, author, sections) {
 
 async function main () {
   try {
-    await resetIndex()
+    await client.resetIndex()
     await readBooks()
   } catch (err) {
     console.error(err)
