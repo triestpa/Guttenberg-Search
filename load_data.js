1f7bb2bf4a96b362af067f6cdb8dcc6d83d6c6fe
triestpa <patrick.triest@gmail.com>
2017-12-03 11:32:28 +0545

    Add commenting to load data script

--- load_data.js
+++ load_data.js
@@ -1,69 +1,77 @@
-const { client, index, type } = require('./connection')
+const esConnection = require('./connection')
 const fs = require('fs')
 const path = require('path')
 
 /** Parse and index all txt files in data directory */
 async function readBooks () {
+  // Read books directory
   let files = fs.readdirSync('./books').filter(file => file.slice(-4) === '.txt')
   console.log(`Found ${files.length} Files`)
+
+  // Read each book file, and index each paragraph in elasticsearch
   for (let file of files) {
     console.log(`Reading File - ${file}`)
     const filePath = path.join('./books', file)
-    const { title, author, sections } = parseBookFile(filePath)
-    await insertBookData(title, author, sections)
+    const { title, author, paragraphs } = parseBookFile(filePath)
+    await insertBookData(title, author, paragraphs)
   }
 }
 
-/** Read an individual book txt file, and extract the title, author, and sections */
+/** Read an individual book txt file, and extract the title, author, and paragraphs */
 function parseBookFile (filePath) {
+  // Read text file
   const book = fs.readFileSync(filePath, 'utf8')
+
+  // Find book title and author
   const title = book.match(/^Title:\s(.+)$/m)[1]
   const authorMatch = book.match(/^Author:\s(.+)$/m)
   const author = (!authorMatch || authorMatch[1].trim() === '') ? 'Unknown Author' : authorMatch[1]
 
   console.log(`Reading Book - ${title} By ${author}`)
 
+  // Find Guttenberg metadata header and footer
   const startOfBookMatch = book.match(/^\*{3}\s*START OF (THIS|THE) PROJECT GUTENBERG EBOOK.+\*{3}$/m)
   const startOfBookIndex = startOfBookMatch.index + startOfBookMatch[0].length
   const endOfBookIndex = book.match(/^\*{3}\s*END OF (THIS|THE) PROJECT GUTENBERG EBOOK.+\*{3}$/m).index
 
-  const sections = book
+  // Clean book text and split into array of paragraphs
+  const paragraphs = book
     .slice(startOfBookIndex, endOfBookIndex) // Remove Guttenberg header and footer
     .split(/\n\s+\n/g) // Split each paragraph into it's own array entry
     .map(line => line.replace(/\r\n/g, ' ').trim()) // Remove paragraph line breaks and whitespace
     .map(line => line.replace(/_/g, '')) // Guttenberg uses "_" to signify italics.  We'll remove it, since it makes the raw text look messy.
     .filter((line) => (line && line.length !== '')) // Remove empty lines
 
-  console.log(`Parsed ${sections.length} Lines\n`)
-  return { title, author, sections }
+  console.log(`Parsed ${paragraphs.length} Lines\n`)
+  return { title, author, paragraphs }
 }
 
 /** Bulk index the book data in ElasticSearch */
-async function insertBookData (title, author, sections) {
+async function insertBookData (title, author, paragraphs) {
   const bulkOps = [] // Array to store bulk operations
 
   // Add an index operation for each section in the book
-  for (let i = 0; i < sections.length; i++) {
+  for (let i = 0; i < paragraphs.length; i++) {
     // Describe action
-    bulkOps.push({ index: { _index: index, _type: type } })
+    bulkOps.push({ index: { _index: esConnection.index, _type: esConnection.type } })
 
     // Add document
     bulkOps.push({
       Author: author,
       Title: title,
       Paragraph: i,
-      Text: sections[i]
+      Text: paragraphs[i]
     })
   }
 
   // Execute bulk ops array
-  await client.bulk({ body: bulkOps })
+  await esConnection.client.bulk({ body: bulkOps })
 }
 
 /** Clear ES index and insert books from filesystem */
 async function main () {
   try {
-    await client.resetIndex()
+    await esConnection.resetIndex()
     await readBooks()
   } catch (err) {
     console.error(err)
