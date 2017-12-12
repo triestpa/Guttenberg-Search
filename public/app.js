131c68981e8178043c15733177483b038ad90cb6
triestpa <patrick.triest@gmail.com>
2017-12-12 11:37:00 +0545

    Split frontend into three distinct files

---
+++ public/app.js
@@ -0,0 +1,90 @@
+const vm = new Vue ({
+  el: '#vue-instance',
+  data () {
+    return {
+      baseUrl: 'http://localhost:3000', // API url
+
+      searchTerm: "Hello World", // Default search term
+      searchDebounce: null, // Timeout for search bar debounce
+      searchResults: [], // Displayed search results
+      numHits: null, // Total search results found
+      searchOffset: 0, // Search result pagination offset
+
+      selectedParagraph: null, // Selected paragraph object
+      bookOffset: 0, // Offset for book paragraphs being displayed
+      paragraphs: [] // Paragraphs being displayed in book preview window
+    }
+  },
+  async created () {
+    this.searchResults = await this.search() // Search for default term
+  },
+  methods: {
+    /** Debounce search input by 100 ms */
+    onSearchInput () {
+      clearTimeout(this.searchDebounce)
+      this.searchDebounce = setTimeout(async () => {
+        this.searchOffset = 0
+        this.searchResults = await this.search()
+      }, 100)
+    },
+    /** Call API to search for inputted term */
+    async search () {
+      const response = await axios.get(`${this.baseUrl}/search`, { params: { term: this.searchTerm, offset: this.searchOffset } })
+      this.numHits = response.data.hits.total
+      return response.data.hits.hits
+    },
+    /** Call the API to get current page of paragraphs */
+    async getParagraphs (bookTitle, offset) {
+      try {
+        this.bookOffset = offset
+        const start = this.bookOffset
+        const end = this.bookOffset + 10
+        const response = await axios.get(`${this.baseUrl}/paragraphs`, { params: { bookTitle, start, end } })
+        return response.data.hits.hits
+      } catch (err) {
+        console.error(err)
+      }
+    },
+    /** Get next page of search results */
+    async nextResultsPage () {
+      if (this.numHits > 10) {
+        this.searchOffset += 10
+        if (this.searchOffset + 10 > this.numHits) { this.searchOffset = this.numHits - 10}
+        this.searchResults = await this.search()
+        document.documentElement.scrollTop = 0
+      }
+    },
+    /** Get previous page of search results */
+    async prevResultsPage () {
+      this.searchOffset -=  10
+      if (this.searchOffset < 0) { this.searchOffset = 0 }
+      this.searchResults = await this.search()
+      document.documentElement.scrollTop = 0
+    },
+    /** Get next page (next 10 paragraphs) of selected book */
+    async nextBookPage () {
+      this.$refs.bookModal.scrollTop = 0
+      this.paragraphs = await this.getParagraphs(this.selectedParagraph._source.title, this.bookOffset + 10)
+    },
+    /** Get previous page (previous 10 paragraphs) of selected book */
+    async prevBookPage () {
+      this.$refs.bookModal.scrollTop = 0
+      this.paragraphs = await this.getParagraphs(this.selectedParagraph._source.title, this.bookOffset - 10)
+    },
+    /** Display paragraphs from selected book in modal window */
+    async showBookModal (searchHit) {
+      try {
+        document.body.style.overflow = 'hidden';
+        this.selectedParagraph = searchHit
+        this.paragraphs = await this.getParagraphs(searchHit._source.title, searchHit._source.location - 5)
+      } catch (err) {
+        console.error(err)
+      }
+    },
+    /** Close the book detail modal */
+    closeBookModal () {
+      document.body.style.overflow = 'auto';
+      this.selectedParagraph = null
+    }
+  }
+})
